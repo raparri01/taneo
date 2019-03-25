d21055a771a3bcde827010d6121da3fb23791002
Rob <robertaparri@gmail.com>
2019-03-24 20:47:38 -0400

    Added an 'add to cart' button and information for amy's salon

--- src/assets/data/activities.js
+++ src/assets/data/activities.js
@@ -25,8 +25,8 @@ export const activities = [
           profilePicture: "http://placehold.it/25",
           firstName: "Matt",
           lastName: "Damon",
-          recommends: true,
-          comment: "The cocktails were wicked good. Our waitress was wicked smart and didn't write down our order"
+          recommends: false,
+          comment: "The cocktails were wicked good but the food was so-so. Our waitress was wicked smart and didn't write down our order. Not my first choice."
         },
         {
           profilePicture: "http://placehold.it/25",
@@ -44,16 +44,39 @@ export const activities = [
   }, {
     id: "2",
     name: "Spa Day",
-    categories: [""],
-    price: 15,
+    categories: ["Spa", "Self Care", "Relaxation", "Salon"],
+    price: 150,
     currency: "USD",
     currencySymbol: "$",
     location: "Amy's Salon",
     stock: 0,
     description:{
-      main: "Manicure and Pedicure at Amy's Salon",
-      details: "Includes a gel manicure and pedicure",
-      value: 35
+      main: "Spa Day followed by a manicure and pedicure at Amy's Salon",
+      long: "Enjoy a three-hour spa day at Amy's Salon. Start with a dip in the hot tub followed by 15 minutes in the steam room followed by your choice of a 1 hour massage or accupuncture session. After your massage you will be given 1 hour to have your choice of any of Amy's many relaxation options, including the sensory deprivation pool, cryotherapy treatment, water massage, and more. Your treatment will be finished off with a gel manicure and pedicure",
+      value: 350,
+      reviews: [
+        {
+          profilePicture: "http://placehold.it/25",
+          firstName: "Rachel",
+          lastName: "Ray",
+          recommends: true,
+          comment: "I cannot say enough good things about Amy's Salon. Hands down the best spa experience of my life!"
+        },
+        {
+          profilePicture: "http://placehold.it/25",
+          firstName: "Reese",
+          lastName: "Witherspoon",
+          recommends: true,
+          comment: "Amy's Salon is one of the best spas I have ever had the pleasure of experiencing. If you get a chance to go there, I highly recommend!"
+        },
+        {
+          profilePicture: "http://placehold.it/25",
+          firstName: "Marie",
+          lastName: "Antoinette",
+          recommends: true,
+          comment: "At first I was worried that the spa treatment was too pricey. After my session, I can tell anyone that it was worth every penny!"
+        }
+      ]
     },
     image: "http://placehold.it/75",
     saved: false
